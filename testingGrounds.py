4496602cb41804555bf0ba6b2206387e28d5ebbf
blakebonasera <60533377+blakebonasera@users.noreply.github.com>
2021-08-17 14:14:16 -0700

    Update testingGrounds.py

--- testingGrounds.py
+++ testingGrounds.py
@@ -1,7 +1,7 @@
 def reverseWordOrder(string):
     var wordArr = string.split(" ")
-    returnString = ""
-    for(var i=wordArr.length-1; i > 0; i--):
+    var returnString = ""
+    for(var i = wordArr.length-1; i > 0; i--)
                 returnString += wordArr[i]
         returnString += " "
     return returnString;
