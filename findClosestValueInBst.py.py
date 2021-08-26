a20180d3d5bd0fe41470eb37386ebdcb194b98fa
blakebonasera <60533377+blakebonasera@users.noreply.github.com>
2021-08-26 09:54:56 -0700

    Update findClosestValueInBst.py.py

--- findClosestValueInBst.py.py
+++ findClosestValueInBst.py.py
@@ -1,3 +1,5 @@
+#Average: O(log(n)) time | O(log(n)) space
+#Worst: O(n) time | O(n) space
 def findClosestValueInBst(tree, target):
     return findClosestValueInBstHelper(tree, target, float("inf"))
 
@@ -13,6 +15,26 @@ def findClosestValueInBstHelper(tree, target, closest):
     else:
         return closest
     
+
+#Average: O(log(n)) time | O(1) space
+#Worst: O(n) time | O(1) space
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree, target, tree.value)
+
+def findClosestValueInBstHelper(tree, target, closest):
+    currentNode = tree
+    while currentNode is not None:
+        if abs(target - closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+        if target < currentNode.value:
+            currentNode = currentNode.left
+        elif target < currentNode.value:
+            currentNode = currentNode.right
+        else:
+            break
+    return closest
+
+
 
 class BST:
     def __init__(self,value):
