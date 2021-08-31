dc71726ba2795bf29366815611bfb44455fa6bc7
blakebonasera <60533377+blakebonasera@users.noreply.github.com>
2021-08-31 09:53:37 -0700

    set up and python solution

---
+++ generateDocument.py
@@ -0,0 +1,20 @@
+#O(m * (n+m)) time | O(1) space
+# of characters and m is the length of the document
+def generateDocument(characters, document):
+    for character in document:
+        documentFrequency = countCharacterFrequency(character,document)
+        charactersFrequency = countCharacterFreuency(character, characters)
+        if documentFrequency > charactersFrequency:
+            return False
+
+    return True
+
+
+
+def countCharacterFrequency(character, target):
+    frequency = 0
+    for char in target:
+        if char == character:
+            frequency += 1
+        
+    return frequency
