33765312c4ee42e83dd62ecd4e0ed837a80f489c
blakebonasera <60533377+blakebonasera@users.noreply.github.com>
2021-08-31 14:45:38 -0700

    Update generateDocument.py

--- generateDocument.py
+++ generateDocument.py
@@ -38,3 +38,23 @@ def generateDocument2(characters, document):
         alreadyCounted.add(character)
     
     return True
+
+# O(n + m) time | O(c) space
+# the length of the document, and c is the nujmber of the unique characters in the document
+
+def generateDocument(characters, document):
+    characterCounts = {}
+    
+    for character in characters:
+        if character not in characterCounts:
+            characterCounts[character] = 0
+        
+        characterCounts[character] += 1
+    
+    for character in document:
+        if character not in characterCounts : characterCounts[character] == 0
+        return False
+        
+    characterCounts[character] -= 1
+
+    return True
